6d9514de2588bb0c57bbc01dbb83bd6ee659442f
angelou6 <68724210+angelou6@users.noreply.github.com>
2026-06-25 09:04:00 -0600

    better error handling

--- src/commands/playlist/join.ts
+++ src/commands/playlist/join.ts
@@ -1,63 +1,40 @@
 import {
   ChatInputCommandInteraction,
-  DiscordAPIError,
-  MessageFlags,
   SlashCommandBuilder,
+  MessageFlags,
 } from "discord.js";
 import {
   entersState,
   joinVoiceChannel,
   VoiceConnectionStatus,
 } from "@discordjs/voice";
-import { getBotVC, getStore, getUserVC } from "../../utils/memory.ts";
-
-class BotInVCError extends Error {
-  constructor() {
-    super("El bot ya esta en un VC");
-    this.name = "BotInVCError";
-    Object.setPrototypeOf(this, BotInVCError.prototype);
-  }
-}
+import { getBotVC, getStore, getUserVC } from "../../utils/store.ts";
+import { BotInVCError, UserNotInVCError } from "../../errors/errors.ts";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("join")
     .setDescription("Hace que el bot se una al vc"),
   async execute(interaction: ChatInputCommandInteraction) {
-    await interaction.deferReply();
+    const store = getStore(interaction);
 
-    try {
-      const store = getStore(interaction);
+    if (await getBotVC(interaction)) throw new BotInVCError();
 
-      if (await getBotVC(interaction)) throw new BotInVCError();
+    const userVC = await getUserVC(interaction);
+    if (!userVC) throw new UserNotInVCError();
 
-      const userVC = await getUserVC(interaction);
-      if (!userVC) throw new Error("UserVC is null");
+    const connection = joinVoiceChannel({
+      channelId: userVC.id,
+      guildId: userVC.guildId,
+      adapterCreator: userVC.guild.voiceAdapterCreator,
+    });
 
-      const connection = joinVoiceChannel({
-        channelId: userVC.id,
-        guildId: userVC.guildId,
-        adapterCreator: userVC.guild.voiceAdapterCreator,
-      });
+    await entersState(connection, VoiceConnectionStatus.Ready, 10_000);
+    connection.subscribe(store.player);
 
-      await entersState(connection, VoiceConnectionStatus.Ready, 10_000);
-      connection.subscribe(store.player);
-
-      await interaction.editReply("Conectado.");
-    } catch (error) {
-      if (error instanceof DiscordAPIError) console.error(error);
-      if (error instanceof BotInVCError) {
-        await interaction.reply({
-          content: "Ya estoy en un VC.",
-          flags: MessageFlags.Ephemeral,
-        });
-        return;
-      }
-
-      await interaction.reply({
-        content: "Connectate a un VC primero.",
-        flags: MessageFlags.Ephemeral,
-      });
-    }
+    await interaction.reply({
+      content: "Conectado.",
+      flags: MessageFlags.Ephemeral,
+    });
   },
 };
--- src/commands/playlist/leave.ts
+++ src/commands/playlist/leave.ts
@@ -1,47 +1,36 @@
 import {
   ChatInputCommandInteraction,
-  MessageFlags,
   SlashCommandBuilder,
+  MessageFlags,
 } from "discord.js";
 import { getVoiceConnection } from "@discordjs/voice";
-import { getStore, userAndBotInSameVC } from "../../utils/memory.ts";
+import { getStore, userAndBotInSameVC } from "../../utils/store.ts";
+import { UnImportantError } from "../../errors/errors.ts";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("leave")
     .setDescription("Hace que el bot se salga del vc"),
   async execute(interaction: ChatInputCommandInteraction) {
-    if (!interaction.inCachedGuild()) {
-      await interaction.reply({
-        content: "This command can only be used in a server.",
-        flags: MessageFlags.Ephemeral,
-      });
-      return;
-    }
+    if (!interaction.inCachedGuild())
+      throw new UnImportantError("Este comando solo puede correr en un Guild");
 
     const store = getStore(interaction);
 
     const connection = getVoiceConnection(interaction.guildId);
     if (connection) {
-      if (!(await userAndBotInSameVC(interaction))) {
-        await interaction.reply({
-          content: "Necesitas estar en el mismo VC que yo",
-          flags: MessageFlags.Ephemeral,
-        });
-        return;
-      }
+      if (!(await userAndBotInSameVC(interaction)))
+        throw new UnImportantError("Necesitas estar en el mismo VC que yo.");
 
       store.player.removeAllListeners("stateChange");
       store.listenerActive = false;
       connection.destroy();
       await interaction.reply({
-        content: "Reply para que discord no se ponga a llorar.",
-      });
-    } else {
-      await interaction.reply({
-        content: "No estoy en un canal de voz",
+        content: "Bye.",
         flags: MessageFlags.Ephemeral,
       });
+    } else {
+      throw new UnImportantError("No estoy en un canal de voz.");
     }
   },
 };
