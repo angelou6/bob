03468790251127bd6dca38f458fc22760b78b7e9
Angelo <68724210+angelou6@users.noreply.github.com>
2026-09-21 07:22:38 -0600

    update tests

--- src/tests/test_spotify.py
+++ src/tests/test_spotify.py
@@ -16,14 +16,30 @@ class TestSpotify(unittest.TestCase):
         data = platform.find_songs(
             "https://open.spotify.com/intl-es/track/5RJPyaKUYv1t45BSdgSKqu"
         )
-        self.assertEqual(data[0].title, "I Don't Want to Be an Engineer")
-        self.assertEqual(data[0].url, "https://www.youtube.com/watch?v=3h7vqzFyKyo")
-        self.assertEqual(data[0].duration, "4:29")
+
+        self.assertEqual(
+            data,
+            [
+                platform.Song(
+                    "I Don't Want to Be an Engineer",
+                    "https://www.youtube.com/watch?v=3h7vqzFyKyo",
+                    "4:29",
+                )
+            ],
+        )
 
     def test_playlist_url(self):
         data = platform.find_songs(
             "https://open.spotify.com/intl-es/album/0M8Foi8rawsI1YHru9bG9B"
         )
-        self.assertEqual(data[0].title, "Stupid Heart")
-        self.assertEqual(data[0].url, "https://www.youtube.com/watch?v=AwLg3sQOqys")
-        self.assertEqual(data[0].duration, "3:10")
+
+        self.assertEqual(
+            data,
+            [
+                platform.Song(
+                    "Stupid Heart",
+                    "https://www.youtube.com/watch?v=AwLg3sQOqys",
+                    "3:10",
+                )
+            ],
+        )
